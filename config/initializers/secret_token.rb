573de42d4ccc9d122b301c92ef102845e7aaaafb
Amritha <amrithap27@gmail.com>
2012-09-02 14:42:45 -0700

    After a bajillion tries I think I can get it right this time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectmadrone::Application.config.secret_token = 'a581afdaf1bfabc1760a72f0b8a37af8e8b907e65d42b151d17b8d8f2f4f83ab14c18ca08742f0d21011678ef0ad585afbcc223cc30eddc6e2ec07ba27235a95'
