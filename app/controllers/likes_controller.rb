4390d134530b0e0c585bc0555f425ba62d3c2219
Amritha Prasad <amrithap27@gmail.com>
2012-09-23 21:29:59 -0700

    Fixing heroku issues with like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,11 @@
 class LikesController < ApplicationController
 	def create
 		#/likes?post_id=5
-	  @like = Like.create(:post_id=> params[:post_id])
+
+		@like= Like.create(:post_id=> params[:post_id], :user=> current_user)	
+	  #@like = Like.new(:post_id=> params[:post_id])
+	  #@like.user = current_user
+	  #@like.save
 	  redirect_to posts_path
 	end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ActiveRecord::Base
-  attr_accessible :post_id, :user_id #these are not protected columns
+  attr_accessible :post_id, :user_id, :user #these are not protected columns
   belongs_to :post
   belongs_to :user 
   validates_presence_of :post_id #validations are at the database level, and checks for post
